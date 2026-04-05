cd22e48ebe860a8bf8b902eef60df3272434784a
tamybk3-ctrl <tamybk3@gmail.com>
2026-04-05 20:27:06 -0300

    feat: add all missing dependencies and fix build

--- dist/sw.js
+++ dist/sw.js
@@ -1,1 +1,6 @@
-if(!self.define){let e,i={};const s=(s,n)=>(s=new URL(s+".js",n).href,i[s]||new Promise(i=>{if("document"in self){const e=document.createElement("script");e.src=s,e.onload=i,document.head.appendChild(e)}else e=s,importScripts(s),i()}).then(()=>{let e=i[s];if(!e)throw new Error(`Module ${s} didn’t register its module`);return e}));self.define=(n,r)=>{const t=e||("document"in self?document.currentScript.src:"")||location.href;if(i[t])return;let o={};const c=e=>s(e,t),l={module:{uri:t},exports:o,require:c};i[t]=Promise.all(n.map(e=>l[e]||c(e))).then(e=>(r(...e),o))}}define(["./workbox-8c29f6e4"],function(e){"use strict";self.skipWaiting(),e.clientsClaim(),e.precacheAndRoute([{url:"registerSW.js",revision:"1872c500de691dce40960bb85481de07"},{url:"index.html",revision:"bd2983518bebe9ef906331f571f0a047"},{url:"assets/index-C6G_3qQV.css",revision:null},{url:"assets/index-Btfoasx-.js",revision:null},{url:"icon-192.png",revision:"99ad6c9011e44447d8a31b836eb2f340"},{url:"manifest.webmanifest",revision:"cac22ccb3516f7e466f0c3b4a774d32b"}],{}),e.cleanupOutdatedCaches(),e.registerRoute(new e.NavigationRoute(e.createHandlerBoundToURL("index.html")))});
+self.addEventListener("install", function(event) {
+  console.log("Service Worker instalado");
+});
+
+self.addEventListener("fetch", function(event) {
+});
